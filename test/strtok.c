a07227088e0a67b14e063c62d6a40f5245b1a432
johnlivingprooff <johnlivingprooff@gmail.com>
2023-09-19 13:34:17 +0200

    Adding a custom strtok function

---
+++ test/strtok.c
@@ -0,0 +1,12 @@
+#include "../sshell.h"
+
+/**
+ * _strtok - tokenises the user commands into arguments
+ * @input: the user input
+ * @delim: the specified delimiters
+ * Return: pointer to the token, or NULL if failed
+ */
+char *_strtok(char *input, char *delim)
+{
+
+}
